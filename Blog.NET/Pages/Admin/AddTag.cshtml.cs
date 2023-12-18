f364a187986bc3cc7456386925abcc540b786181
lsocpb <lukasssonik@gmail.com>
2023-12-18 15:18:16 +0100

    fixed, working stable version

---
+++ Blog.NET/Pages/Admin/AddTag.cshtml.cs
@@ -0,0 +1,45 @@
+using Blog.NET.Data;
+using Blog.NET.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blog.NET.Pages.Admin
+{
+    public class AdminTag : PageModel
+    {
+        private readonly AppDbContext _context;
+
+        [BindProperty]
+        public string Name { get; set; }
+        [BindProperty]
+        public string DisplayName { get; set; }
+
+        public AdminTag(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+
+            var newTag = new Tag
+            {
+                Name = Name,
+                DisplayName = DisplayName
+            };
+
+            _context.Tags.Add(newTag);
+            _context.SaveChanges();
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
