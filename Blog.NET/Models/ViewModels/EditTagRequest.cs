3237f84733326fc7ed870ac81f4e88e32bfc76df
hsup <kontonastatystyke@gmail.com>
2023-12-19 14:52:51 +0100

    Added edit tag admin functionality

---
+++ Blog.NET/Models/ViewModels/EditTagRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Blog.NET.Models.ViewModels;
+
+public class EditTagRequest
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string DisplayName { get; set; }
+}
--- Blog.NET/Pages/Admin/ListTag.cshtml.cs
+++ Blog.NET/Pages/Admin/ListTag.cshtml.cs
@@ -9,12 +9,6 @@ public class ListCshtml : Controller
     public class ListTagModel : Microsoft.AspNetCore.Mvc.RazorPages.PageModel
     {
         private readonly AppDbContext _context;
-
-        [BindProperty]
-        public string Name { get; set; }
-        [BindProperty]
-        public string DisplayName { get; set; }
-
         public ListTagModel(AppDbContext context)
         {
             _context = context;
