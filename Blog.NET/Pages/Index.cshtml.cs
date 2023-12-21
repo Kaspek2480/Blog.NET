755bcef8391c0c8ce404ea7fda7bfdcc41b16b03
hsup <kontonastatystyke@gmail.com>
2023-12-21 12:56:28 +0100

    Commiting index page changes before I break something by adding controllers

--- Blog.NET/Pages/Index.cshtml.cs
+++ Blog.NET/Pages/Index.cshtml.cs
@@ -1,18 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.NET.Data;
+using Blog.NET.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.NET.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly AppDbContext _context;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, AppDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
+    
+    public List<BlogPost>? Posts { get; set; }
 
     public void OnGet()
     {
+        Posts = _context.Blogs.Include(b => b.Tags).ToList();
     }
 }
