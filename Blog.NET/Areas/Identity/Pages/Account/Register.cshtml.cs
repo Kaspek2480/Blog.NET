6b2f86064d21be0b778968bf62c399e7e14c5ff1
lsocpb <lukasssonik@gmail.com>
2023-12-26 20:00:53 +0100

    name instead of username added +user can add post rn

--- Blog.NET/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Blog.NET/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -125,6 +125,8 @@ namespace Blog.NET.Areas.Identity.Pages.Account
 
                 if (result.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(user, "User");
+
                     _logger.LogInformation("User created a new account with password.");
 
                     var userId = await _userManager.GetUserIdAsync(user);
--- Blog.NET/Pages/Index.cshtml.cs
+++ Blog.NET/Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
-﻿using Blog.NET.Data;
+﻿using Blog.NET.Areas.Identity.Data;
+using Blog.NET.Data;
 using Blog.NET.Models;
 using Blog.NET.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,22 +13,31 @@ public class IndexModel : PageModel
     private readonly ILogger<IndexModel> _logger;
     private readonly AppDbContext _context;
     private readonly IBlogPostLikeRepository _blogPostLikeRepository;
+    public readonly UserManager<BlogNETUser> _userManager;
 
-    public IndexModel(ILogger<IndexModel> logger, AppDbContext context, IBlogPostLikeRepository blogPostLikeRepository)
+    public IndexModel(ILogger<IndexModel> logger, AppDbContext context, IBlogPostLikeRepository blogPostLikeRepository, UserManager<BlogNETUser> userManager)
     {
         _logger = logger;
         _context = context;
         _blogPostLikeRepository = blogPostLikeRepository;
+        _userManager = userManager;
     }
     
     public List<Models.BlogPost>? Posts { get; set; }
     
     public List<Tag>? Tags { get; set; }
-    
+
+    public UserManager<BlogNETUser> UserManager { get; }
+
 
     public void OnGet()
     {
         Posts = _context.Blogs.Include(b => b.Tags).OrderByDescending(p => p.CreatedAt).ToList();
         Tags = _context.Tags.ToList();
+    }
+
+    public BlogNETUser GetUserById(string userId)
+    {
+        return _userManager.FindByIdAsync(userId).Result;
     }
 }
