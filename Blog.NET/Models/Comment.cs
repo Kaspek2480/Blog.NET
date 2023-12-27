d49144a3ba1afd40af610bc39ac44f6932b9e01b
hsup <kontonastatystyke@gmail.com>
2023-12-27 11:00:43 +0100

    Implemented add comment functionality

--- Blog.NET/Models/Comment.cs
+++ Blog.NET/Models/Comment.cs
@@ -7,16 +7,13 @@ namespace Blog.NET.Models
     public class Comment
     {
         public int Id { get; set; }
-
         public string Content { get; set; }
+        public Guid BlogPostId { get; set; }
+        public string UserId { get; set; }
         public DateTime CreatedAt { get; set; }
         public string? IPAddress { get; set; }
-
-
-        public string UserId { get; set; }
+        
         public BlogNETUser User {get;  set;}
-
-        public Guid BlogPostId { get; set; }
         public BlogPost BlogPost {  get; set; }
     }
 }
--- Blog.NET/Pages/BlogPost.cshtml.cs
+++ Blog.NET/Pages/BlogPost.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Blog.NET.Data;
+﻿using System.Security.Claims;
+using Blog.NET.Data;
 using Blog.NET.Models;
 using Blog.NET.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -12,44 +13,53 @@ public class BlogPostModel : PageModel
     private readonly AppDbContext _context;
     private readonly IBlogPostLikeRepository _blogPostLikeRepository;
     private readonly IUserRepository _userRepository;
-    
+
     [BindProperty] public BlogPost? BlogPost { get; set; }
     [BindProperty] public int TotalLikes { get; set; }
     [BindProperty] public Comment? Comment { get; set; }
-    
 
-    public BlogPostModel(AppDbContext context, IBlogPostLikeRepository blogPostLikeRepository, IUserRepository userRepository)
+    [BindProperty] public List<Comment>? Comments { get; set; }
+
+
+    public BlogPostModel(AppDbContext context, IBlogPostLikeRepository blogPostLikeRepository,
+        IUserRepository userRepository)
     {
         _context = context;
         _blogPostLikeRepository = blogPostLikeRepository;
         _userRepository = userRepository;
     }
-    
+
     public async Task<IActionResult> OnGet(Guid id)
     {
         var post = await _context.Blogs.Include(blogPost => blogPost.Tags).FirstOrDefaultAsync(p => Equals(p.Id, id));
         if (post == null) return NotFound();
 
+        Comments = await _context.Comments.Include(comment => comment.User).Where(comment => comment.BlogPostId == id)
+            .OrderByDescending(comment => comment.CreatedAt).ToListAsync();
+        
+       
         BlogPost = post; //FIXME: User object in BlogPost is null
         TotalLikes = await _blogPostLikeRepository.GetTotalLikes(id);
-        
+
         return Page();
     }
-    
-    public async Task<IActionResult> OnPostCommentAsync(Guid id, string comment)
+
+    public async Task<IActionResult> OnPost()
     {
-        var post = await _context.Blogs.Include(blogPost => blogPost.Tags).Include(blogPost => blogPost.Comments).FirstOrDefaultAsync(p => Equals(p.Id, id));
-        if (post == null) return NotFound();
-
-        post.Comments.Add(new Comment
-        {
-            Content = comment,
-            CreatedAt = DateTime.UtcNow,
-            // UserId = User.FindFirstValue(ClaimTypes.NameIdentifier),
-        });
-
+        //if (!ModelState.IsValid) return Page();
+        
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var user = await _userRepository.GetCurrentUser();
+        if (user == null) return NotFound();
+        
+        Comment.UserId = userId;
+        //Comment.User = user;
+        Comment.CreatedAt = DateTime.Now;
+        Comment.IPAddress = HttpContext.Connection.RemoteIpAddress?.ToString();
+        Comment.BlogPostId = BlogPost.Id;
+        await _context.Comments.AddAsync(Comment);
         await _context.SaveChangesAsync();
-
-        return RedirectToPage("/BlogPost", new {id});
+        
+        return RedirectToPage("/BlogPost", new { id = Comment.BlogPostId });
     }
 }
